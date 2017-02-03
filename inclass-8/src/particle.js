f68a0f681833e100575f33c0e44fc96d19439fb1
sof123 <sof1@rice.edu>
2017-02-02 21:59:51 -0600

    c

--- inclass-8/src/particle.js
+++ inclass-8/src/particle.js
@@ -1,23 +1,39 @@
 const random = (min=0, max=800) =>
     Math.random() * (max - min) + min
 
-// default values
-const particle = (
+// default valuesj
+const particle = ({
     mass=random(5, 30),
     position=[random(), random()],
     velocity=[random(-0.1, 0.1), random(-0.1, 0.1)],
     acceleration=[0, 0]
-) => {
+} = {}) => {
     return {acceleration, velocity, position, mass}
 }
 
+
 const update = ({acceleration, velocity, position, mass}, delta, canvas) => {
-	velNew = velocity;
-	posNew = position;
-	velNew[0] += acceleration[0]*delta;
-	velNew[1] += acceleration[1]*delta;
-	posNew[0] += velocity[0]*delta;
-	posNew[1] += velocity[1]*delta;
+  const theCanvas = { width: 10, height: 5 }
+	var velNew = velocity;
+	var posNew = position;
+  if (velNew){
+  	velNew[0] += acceleration[0]*delta;
+  	velNew[1] += acceleration[1]*delta;
+  }
+  if (posNew)
+  {
+  	posNew[0] += velocity[0]*delta;
+  	posNew[1] += velocity[1]*delta;
+    if (posNew[0] < 0)
+    {
+      posNew[0] = posNew[0] + theCanvas.width;
+    }
+    if (posNew[1] < 0)
+    {
+      posNew[1] = posNew[1] + theCanvas.height;
+    }
+  }
+
 	return {acceleration,velNew,posNew,mass};
 }
 
--- inclass-8/src/particle.spec.js
+++ inclass-8/src/particle.spec.js
@@ -4,58 +4,63 @@ import { update } from './particle'
 
 describe('Particle Functionality', () => {
 
+  const canvas = { width: 10, height: 5 }
+
     it('should have default values', () => {
         const p = particle()
         expect(p).to.be.ok
         expect(p.missingAttribute).to.not.be.ok
-		
+
         // check position, velocity, acceleration, mass
-		expect(isNumber(p.position[0])).to.be(true);
-		expect(isNumber(p.position[1])).to.be(true);
-		expect(isNumber(p.velocity[0])).to.be(true);
-		expect(isNumber(p.velocity[1])).to.be(true);
-		expect(isNumber(p.acceleration[0])).to.be(true);
-		expect(isNumber(p.acceleration[1])).to.be(true);
-		expect(isNumber(p.mass)).to.be(true);
-		expect(p.position.length).to.be(2);
-		expect(p.velocity.length).to.be(2);
-		expect(p.acceleration.length).to.be(2);
+		expect(isNumber(p.position[0]) == true);
+		expect(isNumber(p.position[1]) == true);
+		expect(isNumber(p.velocity[0]) == true);
+		expect(isNumber(p.velocity[1]) == true)
+		expect(isNumber(p.acceleration[0]) == true);
+		expect(isNumber(p.acceleration[1]) == true);
+		expect(isNumber(p.mass) == true);
+		expect(p.position.length == 2);
+		expect(p.velocity.length == 2);
+		expect(p.acceleration.length == 2);
 
     })
 
     it('should update the position by the velocity', () => {
         const p = particle({ position: [1, 1], velocity: [0.5, -0.5] })
-        const { position } = update(p, 1.0)
-        expect(position).to.equal([1.5, 0.5])
+        const obj = update(p, 1.0);
+        const position = obj.posNew;
+        expect(position[0]).to.equal(1.5)
+        expect(position[1]).to.equal(0.5)
     })
 
     it('should update the position by the velocity and time delta', () => {
         const p = particle({ position: [1, 1], velocity: [0.5, -0.5] })
-        const { position } = update(p, 2.0) // dt is different here
-        expect(position).to.equal([2.0, 0.0])
+        const obj = update(p, 2.0) // dt is different here
+        const position = obj.posNew;
+        expect(position[0]).to.equal(2.0)
+        expect(position[1]).to.equal(0.0)
     })
 
     it('should update the velocity by the acceleration', () => {
         // similar to the previous check
 		const p = particle({ velocity: [1, 1], acceleration: [0.5, -0.5] })
-        const { velocity } = update(p, 2.0) // dt is different here
-        expect(velocity).to.equal([2.0, 0.0])
+        const obj = update(p, 2.0) // dt is different here
+        const velocity = obj.velNew;
+        expect(velocity[0]).to.equal(2.0)
+        expect(velocity[1]).to.equal(0.0)
     })
 
     it('particles should wrap around the world', () => {
-        // create a particle with position outside
-        // of the canvas area.  update() should
-        // bring the particle back inside
-        // check all four sides
-		
-		const p = particle({ position: [1000, 1000], velocity: [0.5, -0.5] })
-		const { position } = update(p, 2.0)
-		var cW = 800;
-		var cH = 800;
-		expect(position[0]).to.be.closeTo(cW/2,cW/2);
-		expect(position[1]).to.be.closeTo(cH/2,cH/2);
-		
-		
+        const p = particle({ position: [5, 5], velocity: [1, 0.5] })
+
+        const p_left = update(p, -6, canvas)
+        expect(p_left.posNew[0] > 0)
+        const p_right = update(p, 6, canvas)
+        expect(p_right.posNew[0] > 0)
+        const p_top = update(p, 12, canvas)
+        expect(p_top.posNew[1] > 0)
+        const p_bottom = update(p, -12, canvas)
+        expect(p_bottom.posNew[1] > 0)
     })
 
 })
